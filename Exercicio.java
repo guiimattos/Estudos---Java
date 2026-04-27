f23ec8625935b30869c23f5afb8d2360a846be25
Guilherme Rodrigues Mattos <guilherme.mattos@eduzz.com>
2026-04-27 13:39:31 -0300

    Format exercise code

--- Exercicio.java
+++ Exercicio.java
@@ -4,6 +4,20 @@ public class Exercicio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Seu nome:");
+
+        System.out.println("Digite o valor da sua compra:");
+        int valor = scanner.nextInt();
+
+        System.out.println("Digite sua idade:");
+
+        System.out.println("Voce e estudante? true/false");
+        boolean estudante = scanner.nextBoolean();
+
+        if (valor >= 500) {
+            System.out.println("desconto de 15%");
+        }
+
         scanner.close();
     }
 }
