a403462be3967b63e893f7827e3ccd926f306baf
Guilherme Rodrigues Mattos <guilherme.mattos@eduzz.com>
2026-04-27 11:33:10 -0300

    ingresso

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Voce tem ingresso? true/false:");
+        boolean temIngresso = scanner.nextBoolean();
+
+        if (idade >= 18 && temIngresso && true ) {
+            System.out.println("Entrada permitida");
+        } else {
+            System.out.println("Entrada negada");
+        }
+
+        scanner.close();
+    }
+}
